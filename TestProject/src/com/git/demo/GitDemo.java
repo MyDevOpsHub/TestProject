0520bf76f54a4442688ee5c12470bd59a6fac6c4
Krishna <krishna@dev.org>
2018-11-24 12:44:42 +0530

    New class push.

---
+++ TestProject/src/com/git/demo/GitDemo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.git.demo;
+
+/**
+ * @author Krishna
+ *
+ */
+public class GitDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
