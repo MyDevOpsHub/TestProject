df5a5c510b0ca93a1e6bb09d29579322d02f79ab
Krishna <krishna@dev.org>
2018-11-24 22:09:46 +0530

    New changes

---
+++ TestProject/src/main/java/com/git/demo/GitDemo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.git.demo;
+
+/**
+ * @author Krishna
+ *
+ */
+public class GitDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello i am with GitHub repo");
+	}
+
+}
